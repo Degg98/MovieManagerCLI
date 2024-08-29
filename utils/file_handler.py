c6e23b6d32f889e893ea408e8355645ffc0a0a63
Degg98 <vincenzo.degiacomo98@gmail.com>
2024-08-29 13:12:48 +0200

    doc: Update file handler classes and factory documentation

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,6 +1,18 @@
 from utils.file_handler_factory import FileHandlerFactory
 
 class FileHandler:
+    """
+    A class that handles loading and saving movies from different sources.
+
+    Args:
+        file_type (str, optional): The type of file to handle. Defaults to "json".
+        db_name (str, optional): The name of the database file. Defaults to "movies.db".
+
+    Methods:
+        load_movies(source): Loads movies from the specified source.
+        save_movies(destination): Saves movies to the specified destination.
+        close(): Closes the file handler.
+    """
     def __init__(self, file_type="json", db_name="movies.db"):
         self.handler = FileHandlerFactory.create_file_handler(file_type, db_name)
 
--- utils/file_handler_factory.py
+++ utils/file_handler_factory.py
@@ -2,6 +2,19 @@ from .json_file_handler import JsonFileHandler
 from .xml_file_handler import XmlFileHandler
 
 class FileHandlerFactory:
+    """
+    Create a file handler based on the given file type.
+
+    Parameters:
+        file_type (str): The type of the file handler to create. Supported types are "json" and "xml".
+        db_name (str): The name of the database file. Default is "movies.db".
+
+    Returns:
+        FileHandler: An instance of the file handler based on the given file type.
+
+    Raises:
+        ValueError: If the given file type is not supported.
+    """
     @staticmethod
     def create_file_handler(file_type, db_name="movies.db"):
         if file_type == "json":
